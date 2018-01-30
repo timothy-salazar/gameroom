51619fd50b0d03a8af01df098817d01720574293
Timothy Salazar <timothy.salazar@colorado.edu>
2018-01-29 19:22:15 -0700

    adding gameroom material

---
+++ light_control/__init__.py
@@ -0,0 +1,1 @@
+from .light_control import app
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='light_control',
+    packages=['light_control'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
