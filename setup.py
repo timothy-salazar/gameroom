de970968fca0baac4a31fa5b72c337bba1f2ebb6
Timothy Salazar <timothy.salazar@colorado.edu>
2018-01-29 23:09:49 -0700

    changing names

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='light_control',
-    packages=['light_control'],
+    name='gameroom_control',
+    packages=['gameroom_control'],
     include_package_data=True,
     install_requires=[
         'flask',
