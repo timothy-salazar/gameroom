67f3999b6679baec42614c396e72f1b99a86de01
Timothy Salazar <timothy.salazar@colorado.edu>
2018-02-12 19:27:58 -0700

    updating with changes to gameroom_control.py, light_adjust.html template. slider bar functionality, functional rf outlets

--- gameroom_control/gameroom_control.py
+++ gameroom_control/gameroom_control.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 import pigpio
 import numpy as np
 import time
-from light_control.transmitRF import transmit_code, transmit_code_et
+from light_control.transmitRF import transmit_code, transmit_outlet
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
@@ -21,123 +21,72 @@ PASSWORD='default'
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 pi1 = pigpio.pi()
 
-# def transmit_code(a):
-#     print(a)
-#
-# def transmit_code_et(a):
-#     print(a)
-#
-# @app.route('/a_on')
-# def a_on():
-#     transmit_code('a_on')
-#     return render_template('layout.html')
-#
-# @app.route('/a_off')
-# def a_off():
-#     transmit_code('a_off')
-#     return render_template('layout.html')
-#
-# @app.route('/red_on')
-# def red_on():
-#     transmit_code_et('two_on')
-#     return render_template('layout.html')
-#
-# @app.route('/red_off')
-# def red_off():
-#     transmit_code_et('two_off')
-#     return render_template('layout.html')
+@app.route('/outlet', methods=['POST'])
+def outlet():
+    code_dict = {1:['two_on','two_off'],2:['three_on','three_off'],3:['four_on','four_off']}
+    out = [request.form['one'],request.form['two'],request.form['three']]
+    print(request.form)
+    on_arr = np.where([i=='on' for i in a])[0]
+    off_arr = np.where([i=='off' for i in a])[0]
+    if len(on_arr)==1:
+        print(code_dict[on_arr])
+        transmit_outlet(code_dict[on_arr][0])
+    if len(off_arr)==1:
+        print(code_dict[on_arr])
+        transmit_outlet(code_dict[on_arr][1])
+    return render_template('light_adjust.html',l_mode='light_controls')
 
-# @app.route('/change_ceiling',methods=['GET','POST'])
-# def change_ceiling():
-#     if request.method == 'POST':
-#         if not session.get('logged_in'):
-#             abort(401)
-#         try:
-#             new_colors = np.array([request.form['red'],request.form['green'],request.form['blue']])
-#             new_colors = new_colors.astype('int')
-#             assert np.all(new_colors<256)
-#             assert np.all(new_colors>=0)
-#             # pi1.set_PWM_dutycycle(5,new_colors[0])
-#             # pi1.set_PWM_dutycycle(13,new_colors[1])
-#             # pi1.set_PWM_dutycycle(26,new_colors[2])
-#             print(new_colors)
-#         except (ValueError,AssertionError):
-#             flash('You need to type a value between 0 and 255 for all boxes')
-#     return render_template('light_adjust.html',l_mode = 'change_ceiling')
-
-# @app.route('/color_wheel',methods=['GET','POST'])
-# def color_wheel():
-#     if request.method == 'POST':
-#         if not session.get('logged_in'):
-#             abort(401)
-#         try:
-#             print(request.form)
-#             print(len(request.form))
-#             for i in request.form.items():
-#                 print(i)
-#             h = request.form['ceiling'].lstrip('#')
-#             print(print('RGB =', tuple(int(h[i:i+2], 16) for i in (0, 2 ,4))))
-#             h = request.form['table'].lstrip('#')
-#             print(print('RGB =', tuple(int(h[i:i+2], 16) for i in (0, 2 ,4))))
-#             # new_colors = np.array([request.form['red'],request.form['green'],request.form['blue']])
-#             # new_colors = new_colors.astype('int')
-#             # assert np.all(new_colors<256)
-#             # assert np.all(new_colors>=0)
-#             # pi1.set_PWM_dutycycle(5,new_colors[0])
-#             # pi1.set_PWM_dutycycle(13,new_colors[1])
-#             # pi1.set_PWM_dutycycle(26,new_colors[2])
-#             #print(new_colors)
-#             return render_template('light_adjust.html',l_mode = 'change_ceiling')
-#         except (ValueError,AssertionError):
-#             flash('You need to type a value between 0 and 255 for all boxes')
-#     return render_template('light_adjust.html',l_mode = 'change_ceiling')
-
-# @app.route('/change_table',methods=['GET','POST'])
-# def change_table():
-#     if request.method == 'POST':
-#         if not session.get('logged_in'):
-#             abort(401)
-#             try:
-#                 new_colors = np.array([request.form['red'],request.form['green'],request.form['blue']])
-#                 new_colors = new_colors.astype('int')
-#                 assert (np.all(new_colors<256) and np.all(new_colors>=0))
-#                 # pi1.set_PWM_dutycycle(17,new_colors[0])
-#                 # pi1.set_PWM_dutycycle(22,new_colors[1])
-#                 # pi1.set_PWM_dutycycle(18,new_colors[2])
-#                 print(new_colors)
-#             except (ValueError,AssertionError):
-#                 flash('You need to type a value between 0 and 255 for all boxes')
-#                 return render_template('light_adjust.html',l_mode='change_table')
-
-# @app.route('/change_both',methods=['GET','POST'])
-# def change_both():
-#     if request.method == 'POST':
-#         if not session.get('logged_in'):
-#             abort(401)
-#             try:
-#                 new_colors = np.array([request.form['red'],request.form['green'],request.form['blue']])
-#                 new_colors = new_colors.astype('int')
-#                 assert (np.all(new_colors<256) and np.all(new_colors>=0))
-#                 # pi1.set_PWM_dutycycle(5,new_colors[0])
-#                 # pi1.set_PWM_dutycycle(13,new_colors[1])
-#                 # pi1.set_PWM_dutycycle(26,new_colors[2])
-#                 # pi1.set_PWM_dutycycle(17,new_colors[0])
-#                 # pi1.set_PWM_dutycycle(22,new_colors[1])
-#                 # pi1.set_PWM_dutycycle(18,new_colors[2])
-#                 print(new_colors)
-#             except (ValueError,AssertionError):
-#                 flash('You need to type a value between 0 and 255 for all boxes')
-#                 return render_template('light_adjust.html',l_mode='change_both')
-#
-@app.route('/a_on')
+@app.route('/a_on', methods=['POST'])
 def a_on():
-    transmit_code('a_on')
+    transmit_outlet('two_on')
     return render_template('light_adjust.html',l_mode='light_controls')
 
-@app.route('/a_off')
+@app.route('/a_off', methods=['POST'])
 def a_off():
-    transmit_code('a_off')
+    transmit_outlet('two_off')
     return render_template('light_adjust.html',l_mode='light_controls')
+
+@app.route('/outlet_2', methods=['POST'])
+def outlet_2():
+    if request.form['two'] == "On":
+        transmit_outlet('two_on')
+        return render_template('light_adjust.html',l_mode='light_controls')
+    elif request.form['two'] == "Off":
+        transmit_outlet('two_off')
+        return render_template('light_adjust.html',l_mode='light_controls')
+        
+# @app.route('/b_off', methods=['POST'])
+# def b_off():
+#     transmit_code('two_off')
+#     return render_template('light_adjust.html',l_mode='light_controls')
+
+@app.route('/outlet_3', methods=['POST'])
+def outlet_3():
+    if request.form['three'] == "On":
+        transmit_outlet('three_on')
+        return render_template('light_adjust.html',l_mode='light_controls')
+    elif request.form['three'] == "Off":
+        transmit_outlet('three_off')
+        return render_template('light_adjust.html',l_mode='light_controls')
+
+# @app.route('/c_off', methods=['POST'])
+# def c_off():
+#     transmit_code('three_off')
+#     return render_template('light_adjust.html',l_mode='light_controls')
+
+@app.route('/outlet_4', methods=['POST'])
+def outlet_4():
+    if request.form['four'] == "On":
+        transmit_outlet('four_on')
+        return render_template('light_adjust.html',l_mode='light_controls')
+    elif request.form['four'] == "Off":
+        transmit_outlet('four_off')
+        return render_template('light_adjust.html',l_mode='light_controls')
+
+# @app.route('/d_off', methods=['POST'])
+# def d_off():
+#     transmit_code('four_off')
+#     return render_template('light_adjust.html',l_mode='light_controls')
 
 @app.route('/', methods=['GET','POST'])
 def light_controls():
@@ -151,25 +100,24 @@ def light_controls():
             new_colors = np.array([request.form['red'],request.form['green'],request.form['blue']])
             new_colors = new_colors.astype('int')
             assert (np.all(new_colors<256) and np.all(new_colors>=0))
-            r = request.form['mode']
-            if (r=='ceiling') or (r=='both'):
+            radio = request.form['mode']
+            if (radio=='ceiling') or (radio=='both'):
                 pi1.set_PWM_dutycycle(5,new_colors[0])
                 pi1.set_PWM_dutycycle(13,new_colors[1])
                 pi1.set_PWM_dutycycle(26,new_colors[2])
                 r = pi1.get_PWM_dutycycle(5)
                 g = pi1.get_PWM_dutycycle(13)
                 b = pi1.get_PWM_dutycycle(26)
-            if (r=='table') or (r=='both'):
+            if (radio=='table') or (radio=='both'):
                 pi1.set_PWM_dutycycle(17,new_colors[0])
                 pi1.set_PWM_dutycycle(22,new_colors[1])
                 pi1.set_PWM_dutycycle(18,new_colors[2])
                 r = pi1.get_PWM_dutycycle(17)
                 g = pi1.get_PWM_dutycycle(22)
-                b = pi1.get_PWM_dutycycle(28)
+                b = pi1.get_PWM_dutycycle(18)
             print(request.form)
         except (ValueError,AssertionError):
             flash('You need to type a value between 0 and 255 for all boxes')
-            #return render_template('light_adjust.html',l_mode='change_both')
     return render_template('light_adjust.html',l_mode='light_controls',r_value=r,g_value=g,b_value=b)
 
 @app.route('/add', methods=['POST'])
